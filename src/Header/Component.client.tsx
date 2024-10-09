c869594bfbb5e9d6a5e251517bfbd6bddaa723e6
Colin Herzog <colin.herzog@codin.ch>
2024-10-09 18:18:46 +0200

    fix: navbar dropdown

--- src/Header/Component.client.tsx
+++ src/Header/Component.client.tsx
@@ -1,147 +1,92 @@
 'use client'
 import React, { useState, useEffect, useRef } from 'react'
 
-import type { Footer, Header, Media } from '@/payload-types'
-import Link from 'next/link'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faBars, faChevronDown, faTimes } from '@awesome.me/kit-693429779d/icons/classic/solid'
+import type { Footer, Header, Media, Page } from '@/payload-types'
 import Image from 'next/image'
 import { CMSLink } from '@/components/Link'
+import { MegaMenu, Navbar } from 'flowbite-react'
+import { usePathname } from 'next/navigation'
 
 interface HeaderClientProps {
   header: Header | Footer
 }
 
 export const HeaderClient: React.FC<HeaderClientProps> = ({ header }) => {
-  const [isOpen, setIsOpen] = useState(false)
-  const [dropdownOpen, setDropdownOpen] = useState(false)
-  const dropdownRef = useRef<HTMLDivElement>(null)
+  const [theme, setTheme] = useState<string | null>(null)
+  const pathname = usePathname()
 
-  const toggleMenu = () => {
-    setIsOpen(!isOpen)
-    setDropdownOpen(false) // Ensure dropdown is closed when the mobile menu is toggled
-  }
-
-  const toggleDropdown = () => {
-    setDropdownOpen(!dropdownOpen)
-  }
-
-  const handleClickOutside = (event: MouseEvent) => {
-    if (dropdownRef.current && !dropdownRef.current.contains(event.target as Node)) {
-      setDropdownOpen(false)
-    }
-  }
-
-  useEffect(() => {
-    if (dropdownOpen) {
-      document.addEventListener('mousedown', handleClickOutside)
-    } else {
-      document.removeEventListener('mousedown', handleClickOutside)
-    }
-
-    return () => {
-      document.removeEventListener('mousedown', handleClickOutside)
-    }
-  }, [dropdownOpen])
-
-  if (!header.navItems) {
-    return (<>y</>)
+  function getHref(link: {
+    type?: ('reference' | 'custom') | null
+    newTab?: boolean | null
+    reference?: {
+      relationTo: 'pages'
+      value: string | Page
+    } | null
+    url?: string | null
+    label: string
+  }) {
+    const href = link.type === 'reference' && typeof link.reference?.value === 'object' && link.reference.value.slug ? `${link.reference?.relationTo !== 'pages' ? `/${link.reference?.relationTo}` : ''}/${link.reference.value.slug}` : link.url
+    return href || ''
   }
 
   header = header as Header
-  return (
-    <header className="z-50 relative">
-      <nav className="bg-background text-foreground">
-        <div className="container flex justify-between items-center py-4">
-          <div className="text-2xl font-bold">
-            <Link href="/">
-              <Image
-                src={'TVWWIL00000002/' + (header.logo as Media).filename}
-                className="mr-3 h-9 flex"
-                width={80}
-                height={60}
-                alt={(header.logo as Media).alt}
-              />
-            </Link>
-          </div>
-          <div className="hidden md:flex space-x-6">
-            {header.navItems?.map((navItem, index) => (
-              navItem.type === 'link' ? (
-                  <CMSLink {...navItem.link} appearance="navBarSimpleLink" className="z-20" key={index} />) :
-                (
-                  <div className="relative" key={index} ref={dropdownRef}>
-                    <button
-                      onClick={toggleDropdown}
-                      className="hover:text-primary-foreground flex items-center"
-                    >
-                      {navItem.dropdownTitle} <FontAwesomeIcon icon={faChevronDown} className="ml-2" />
-                    </button>
-                    {dropdownOpen && (
-                      <div className="absolute left-0 mt-2 w-48 rounded-lg shadow-lg bg-card text-card-foreground z-10">
-                        {navItem.dropdownLinks?.map((dropdownLink, dropdownIndex) => (
-                          <CMSLink
-                            {...dropdownLink.link}
-                            appearance="dropdownLink"
-                            className="z-20"
-                            key={dropdownIndex}
-                            onClickHandler={() => setDropdownOpen(false)}
-                          />
-                        ))}
-                      </div>
-                    )}
+
+  return (<header>
+    <MegaMenu fluid className="bg-blue-800 text-white">
+      <Navbar.Brand href="/">
+        <Image
+          src={'TVWWIL00000002/' + (header.logo as Media).filename}
+          className="mr-3 h-9 flex"
+          width={80}
+          height={40}
+          alt={(header.logo as Media).alt}
+        />
+        <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white"></span>
+      </Navbar.Brand>
+      <div className="flex items-center gap-3 lg:order-2">
+        <Navbar.Toggle theme={{ icon: 'h-5 w-5 shrink-0 ' }} className="z-10 text-white color-white hover:bg-blue-600 " />
+      </div>
+      <Navbar.Collapse
+        theme={{
+          list: 'mt-4 flex flex-col md:mt-0 md:flex-row md:space-x-8 md:text-base [&_a]:font-medium',
+        }}
+        className="md:order-1"
+      >
+        {header.navItems?.map((navItem, index) => (<>
+          {navItem.type === 'link' ? (<Navbar.Link href={getHref(navItem.link!)} className="z-10 relative text-white hover:bg-blue-600 ">
+            {navItem.link?.label}
+          </Navbar.Link>) : (<li className="z-20">
+            <div
+              className="block py-2 pr-4 pl-3 font-semibold text-white border-b border-gray-100 lg:hover:bg-transparent lg:border-0 lg:hover:text-primary-600 lg:p-0 dark:text-gray-400 lg:dark:hover:text-primary-500 dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700 hover:bg-blue-600">
+              <MegaMenu.Dropdown
+                toggle={<span>{navItem.dropdownTitle}</span>}
+                className="z-10 max-w-[90vw]"
+              >
+                <div
+                  className="grid w-full bg-white dark:border-gray-700 dark:bg-gray-700 lg:w-auto lg:grid-cols-3 lg:rounded-lg">
+                  <div
+                    className="bg-white p-2 text-gray-900 dark:bg-gray-800 dark:text-white lg:col-span-2 lg:rounded-lg">
+                    <ul>
+                      {navItem.dropdownLinks?.map((dropdownLink, dropdownLinkIndex) => (<li key={dropdownLinkIndex}>
+                        <a
+                          href={getHref(dropdownLink.link)}
+                          className="flex items-center rounded-lg p-3 hover:bg-gray-50 dark:hover:bg-gray-700"
+                        >
+                          <div>
+                            <div className="font-semibold">
+                              {dropdownLink.link.label}
+                            </div>
+                          </div>
+                        </a>
+                      </li>))}
+                    </ul>
                   </div>
-                )
-            ))}
-          </div>
-          <div className="md:hidden">
-            <button onClick={toggleMenu}>
-              {isOpen ? (<FontAwesomeIcon icon={faTimes} className="h-6 w-6" />) : (
-                <FontAwesomeIcon icon={faBars} className="h-6 w-6" />)}
-            </button>
-          </div>
-        </div>
-
-        {/* Mobile Menu */}
-        {isOpen && (
-          <div className="md:hidden">
-            {header.navItems?.map((navItem, index) => (
-              navItem.type === 'link' ? (
-                <CMSLink
-                  {...navItem.link}
-                  appearance="navBarSimpleLinkMobile"
-                  className="block px-4 py-2 hover:bg-muted"
-                  key={index}
-                  onClickHandler={() => setIsOpen(false)}
-                />
-              ) : (
-                <div key={index} className="relative">
-                  <button
-                    onClick={toggleDropdown}
-                    className="block w-full text-left px-4 py-2 hover:bg-muted"
-                  >
-                    {navItem.dropdownTitle}
-                  </button>
-                  {dropdownOpen && (
-                    <div className="px-4">
-                      {navItem.dropdownLinks?.map((dropdownLink, dropdownIndex) => (
-                        <CMSLink
-                          {...dropdownLink.link}
-
-                          appearance="dropdownLinkMobile"
-                          className="block px-4 py-2 hover:bg-muted"
-                          key={dropdownIndex}
-                          onClickHandler={() => setIsOpen(false)}
-                        />
-                      ))}
-                    </div>
-                  )}
                 </div>
-              )
-            ))}
-          </div>
-        )}
-
-      </nav>
-    </header>
-  )
+              </MegaMenu.Dropdown>
+            </div>
+          </li>)}
+        </>))}
+      </Navbar.Collapse>
+    </MegaMenu>
+  </header>)
 }
