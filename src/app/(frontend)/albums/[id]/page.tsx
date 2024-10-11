f87c8ab8b6296df318e75a0969b232cb0fa4488e
Colin Herzog <colin.herzog@codin.ch>
2024-10-11 12:01:27 +0200

    impl: split client and server

--- src/app/(frontend)/albums/[id]/page.tsx
+++ src/app/(frontend)/albums/[id]/page.tsx
@@ -1,11 +1,12 @@
 import AlbumPhotosClient from './AlbumPhotosClient';
 
+// Ensure that `params` is passed as a Promise and awaited
 type Args = {
-  params: { id: string };
+  params: Promise<{ id: string }>;
 };
 
 export default async function AlbumPhotosPage({ params }: Args) {
-  const { id } = params; // Extract the album id from the params
+  const { id } = await params;  // Await the params, extracting the album id
 
   return (
     <div>
